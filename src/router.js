a12f2a6e2e9d697cf42e2814def33d1e8be35d1d
Michał Bachta <michal.bachta@outlook.com>
2018-09-17 14:20:18 +0200

    removed hash ('#') from url

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import Alphabet from './views/alphabet/Alphabet.vue';
 Vue.use(Router);
 
 export default new Router({
+    mode: 'history',
     routes: [
         {
             path: '/',
--- src/views/menu/Menu.vue
+++ src/views/menu/Menu.vue
@@ -2,7 +2,7 @@
 <div class="menu">
     <ul class="menu-list">
         <li class="menu-item" v-for="(page, index) in pages" :key="index">
-            <a :href="'#' + page.path">
+            <a :href="page.path">
                 {{ page.name.toUpperCase() }}
                 </a>
         </li>
