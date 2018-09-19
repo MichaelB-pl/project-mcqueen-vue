2753f4ff93f8f52486bb9b9d6c8d739c8adcb7b3
Michał Bachta <michalbachta@gmail.com>
2018-09-19 09:48:20 +0200

    menu page  init

--- src/router.js
+++ src/router.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Menu from './views/menu/Menu.vue'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    
+    {
+      path: '/',
+      name: 'app-menu',
+      component: Menu
+    }
   ]
 })
