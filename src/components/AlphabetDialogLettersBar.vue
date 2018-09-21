a83c3eb0d4671ead2074d310af6523af95e1fdc0
Michał Bachta <michalbachta@gmail.com>
2018-09-21 13:01:59 +0200

    AlphabetDialogLettersBar refactor

--- src/components/AlphabetDialogLettersBar.vue
+++ src/components/AlphabetDialogLettersBar.vue
@@ -1,33 +1,24 @@
 <template>
-<div class="letters-bar">
-
-</div>
+<div class="alphabet__dialog-letters-bar" />
 </template>
 
 <script>
 export default {
-    name: 'AlphabetDialogLettersBar',
-    props: {
-        areUnnecessaryComponentsVisible: {
-            type: Boolean,
-            required: true
-        }
-    }
+    name: 'AlphabetDialogLettersBar'
 }
 </script>
 
 <style scoped>
-.letters-bar {
+.alphabet__dialog-letters-bar {
     width: 100%;
     height: var(--bar-size);
     background-color: var(--color-primary);
     box-shadow: 0 0 16px 0 black;
     z-index: 205;
-    transition: height var(--transition-duration);
 }
 
 @media screen and (orientation:landscape) and (max-height: 480px) {
-    .letters-bar {
+    .alphabet__dialog-letters-bar {
         height: calc(var(--bar-size) * 0.8);
     }
 }
