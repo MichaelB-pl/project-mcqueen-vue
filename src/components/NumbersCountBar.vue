430496548d1411805ff5f7ddf883cae2d50b51c2
Michał Bachta <michalbachta@gmail.com>
2018-09-26 14:58:53 +0200

    NumbersCountBar

--- src/components/NumbersCountBar.vue
+++ src/components/NumbersCountBar.vue
@@ -1,15 +1,83 @@
 <template>
-    <div class="app-bar">
-
+<div class="app-bar numbers__count-bar noselect">
+    <div v-for="number in numbersLowerAndEquealThanSelected" 
+        :key="number"
+        :class="'numbers__count-bar-item'"
+        @click="onSelectedNumber(number)"
+        >{{ number }}
     </div>
+    
+    <div v-for="number in numbersHigherThanSelected" 
+        :key="number"
+        :class="'numbers__count-bar-item numbers__count-bar-item-not-selected'"
+        @click="onSelectedNumber(number)"
+        >{{ number }}
+    </div>
+</div>
 </template>
 
 <script>
-    export default {
-        name:'NumbersCountBar'
+import {
+    createNamespacedHelpers, mapActions
+} from 'vuex';
+
+const {
+    mapState,
+    mapGetters,
+    mapMutations
+} = createNamespacedHelpers('numbers');
+
+export default {
+    name: 'NumbersCountBar',
+    computed: {
+        numbersLowerAndEquealThanSelected(state) {
+            return state.numbers.filter(number =>
+                number <= state.selectedNumber);
+        },
+        numbersHigherThanSelected(state) {
+            return state.numbers.filter(number =>
+                number > state.selectedNumber);
+        },
+        ...mapState([
+            'numbers'
+        ]),
+        ...mapGetters([
+            'selectedNumber'
+        ]),
+    },
+    methods: {
+        onSelectedNumber(number) {
+            const index =number - 1;
+            this.selectNumberIndex(index);
+        },
+        ...mapMutations([
+            'selectNumberIndex'
+        ])
     }
+}
 </script>
 
 <style scoped>
+.numbers__count-bar {
+    box-shadow: 0 0 16px 0 black;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+    align-items: center;
+}
 
+.numbers__count-bar-item {
+    font-size: 2em;
+    font-weight: bolder;
+}
+
+.numbers__count-bar-item-not-selected {
+    color: #fff7;
+}
+
+@media(max-width: 480px){
+    .numbers__count-bar-item {
+        font-size: 1.5em;
+    }
+}
 </style>
--- src/store/modules/numbers.js
+++ src/store/modules/numbers.js
@@ -1,11 +1,17 @@
 const numbersModule = {
+    namespaced: true,
     state: {
-        selectedNumberIndex: 0,
+        selectedNumberIndex: 3,
         numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     },
     getters: {
         selectedNumber: state =>
             state.numbers[state.selectedNumberIndex]
+    },
+    mutations: {
+        selectNumberIndex(state, index){
+            state.selectedNumberIndex = index;
+        }
     }
 };
 
