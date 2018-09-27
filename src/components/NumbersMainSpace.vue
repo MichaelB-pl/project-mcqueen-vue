a99cbfd5443e781f712fbb365f6cbdf62e49662a
Michał Bachta <michalbachta@gmail.com>
2018-09-27 16:04:34 +0200

    NumbersMainSpace

--- src/components/NumbersMainSpace.vue
+++ src/components/NumbersMainSpace.vue
@@ -1,22 +1,73 @@
 <template>
 <div class="numbers__main-space">
+    <transition-group name="pop-image">
+        <NumbersMainSpaceItem v-for="number in selectedNumber" :key="number" />
+    </transition-group>
 
 </div>
 </template>
 
 <script>
+import {
+    createNamespacedHelpers
+} from 'vuex';
+
+const {
+    mapGetters
+} = createNamespacedHelpers('numbers');
+
 import NumbersMainSpaceItem from './NumbersMainSpaceItem.vue';
 
 export default {
     name: 'NumbersMainSpace',
     components: {
         NumbersMainSpaceItem
+    },
+    computed: {
+        ...mapGetters([
+            'selectedNumber'
+        ]),
     }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .numbers__main-space {
+        width: 100%;
+        height: calc(100% - var(--bar-size));
+        z-index: 1;
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: center;
+        align-items: center;
 
+    span {
+        width: 100%;
+        height: 100%;
+        z-index: 1;
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: center;
+        align-items: center;
+    }
+}
+
+.pop-image-enter-active,
+.pop-image-leave-active {
+    transition: transform var(--transition-duration), opacity var(--transition-duration);
+}
+
+.pop-image-leave-active {
+    position: absolute;
+}
+
+.pop-image-move {
+    transition: transform var(--transition-duration), opacity var(--transition-duration);
+}
+
+.pop-image-enter,
+.pop-image-leave-to {
+    transform: scale(0.01);
+    opacity: 0;
 }
 </style>
--- src/components/NumbersMainSpaceItem.vue
+++ src/components/NumbersMainSpaceItem.vue
@@ -1,6 +1,6 @@
 <template>
-<div>
-
+<div class="numbers__main-space-item">
+    <img src="https://image.flaticon.com/icons/svg/135/135706.svg"/>
 </div>
 </template>
 
@@ -10,6 +10,28 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+:root {
+    --screen-ratio: 1vmax / 1vmin;
+    --items-count-in-line: 2;
+    --item-size: calc((100vmin - var(--bar-size)) / var(--items-count-in-line));
+    --lines-count: 5;
+    --line-basis: calc((100vmax - var(--bar-size)) / var(--lines-count));
+    --item-flex-basis: calc(var(--line-basis));
+}
 
+.numbers__main-space-item {
+    flex-basis: var(--item-flex-basis);
+    width: var(--item-flex-basis);
+    height: var(--item-flex-basis);
+    display: flex;
+    flex-flow: column norap;
+    justify-content: center;
+    align-items: center;
+
+    img {
+        max-width: 90%;
+        max-height: 90%;
+    }
+}
 </style>
