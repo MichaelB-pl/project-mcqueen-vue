4873d9a2daa282d8794846389afe9f8eb4fed022
Michał Bachta <michalbachta@gmail.com>
2018-09-28 10:04:50 +0200

    AddingPuzzleSpaceItem init

--- src/components/AddingPuzzleSpace.vue
+++ src/components/AddingPuzzleSpace.vue
@@ -1,15 +1,25 @@
 <template>
-<div>
+<div class="adding__puzzle-space">
 
 </div>
 </template>
 
 <script>
+import AddingPuzzleSpace from './AddingPuzzleSpace.vue';
+
 export default {
-    name: 'AddingQuestionSpace'
+    name: 'AddingPuzzleSpace',
+    components: {
+        AddingPuzzleSpace
+    }
 }
 </script>
 
 <style scoped>
+.adding__puzzle-space {
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+}
 
 </style>
---
+++ src/components/AddingPuzzleSpaceItem.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'AddingPuzzleSpaceItem'
+}
+</script>
+
+<style scoped>
+
+</style>
