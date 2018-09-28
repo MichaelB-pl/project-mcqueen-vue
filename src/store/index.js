7c3bed2e436e046fb38ae1cd0e42487198971377
Michał Bachta <michalbachta@gmail.com>
2018-09-28 10:43:13 +0200

    adding store module

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import alphabet from './modules/alphabet';
+import adding from './modules/adding';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    alphabet
+    alphabet,
+    adding
   }
 });
